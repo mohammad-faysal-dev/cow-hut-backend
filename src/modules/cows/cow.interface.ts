4d561fa03633a52b4ef58635c112d61022f1148f
Faysal2123 <foysal2005n1@gmail.com>
2026-06-13 20:51:53 +0600

    feat: add cow interface and constants

---
+++ src/modules/cows/cow.interface.ts
@@ -0,0 +1,14 @@
+import { Types } from "mongoose";
+import { Breed, Category, Level, Location } from "./cow.constant";
+
+export interface ICow {
+    name: string,
+    age: number,
+    price: number,
+    weight: number,
+    location: Location,
+    level: Level,
+    breed: Breed,
+    category: Category,
+    seller: Types.ObjectId;
+}
